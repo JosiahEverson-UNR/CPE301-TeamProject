2fc41fc6d754ec4af0aba25648867129c2628826
“alfrancisd” <cisdeverashs1516@gmail.com>
2020-12-03 12:03:30 -0800

    I added ADC registers and functions (copy and pasted from my lab) ~Francis

--- CPE301_Project.c
+++ CPE301_Project.c
@@ -13,6 +13,14 @@ volatile unsigned char* myPORT_F = (unsigned char*) 0x31;
 volatile unsigned char* myDDR_F  = (unsigned char*) 0x30;
 volatile unsigned char* myPIN_F  = (unsigned char*) 0x2F;
 
+//ADC Registers
+volatile unsigned char* my_ADMUX     = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRA    = (unsigned char*) 0x7A;
+volatile unsigned char* my_ADCSRB    = (unsigned char*) 0x7B;
+volatile unsigned int*  my_ADC_DATA  = (unsigned int*)  0x78;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 //water level threshold
 #define w_threshold 200
 //temperature threshold
@@ -163,7 +171,123 @@ void loop()
     }
   }
 }
-/*      Serial.println("Increment by 1.");
+
+
+
+
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//ADC Functions
+
+void adc_init()
+{
+  //setup the A register
+
+  //set bit 7 to 1 to enable the ADC
+  *my_ADCSRA |= 0x80;
+  // 0000 0000 | 1000 0000 = 1000 0000
+
+  //clear bit 5 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0xDF;
+  // 1000 0000 & 1101 1111 = 1000 0000
+
+  //clear bit 3 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0xF7;
+  // 1000 0000 & 1111 0111 = 1000 0000
+
+  //clear bits 2-0 to 0 to set the prescaler selection to slow reading
+  *my_ADCSRA &= 0xFB;
+  // 1000 0000 & 1111 1011
+
+  //~~~~~~~~~~~~~~
+
+  //setup the B register
+
+  //clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0xF7;
+  // 0000 0000 & 1111 0111 = 0000 0000
+
+  //clear bit 2 to 0 to set the prescaler selection to slow reading
+  *my_ADCSRB &= 0xFB;
+  // 0000 0000 & 1111 1011 = 0000 0000
+
+  //~~~~~~~~~~~~~~~
+
+  //setup the MUX Register
+
+  //set bit 7 to 0 for AVCC analog reference
+  *my_ADMUX &= 0x00;
+  // 0000 0000 & 0000 0000 = 0000 0000
+
+  //set bit 6 to 1 for AVCC analog reference
+  *my_ADMUX |= 0x40;
+  // 0000 0000 | 0100 0000 = 0100 0000
+
+  //clear bit 5 to 0 for right adjust result
+  *my_ADMUX &= 0xDF;
+  // 0100 0000 & 1101 1111 = 0100 0000
+
+  //clear bit 4-0 to 0 to reset the channel and gain bits
+  *my_ADMUX &= 0xE0;
+  // 0100 0000 & 1110 0000 = 0100 0000
+}
+
+  //~~~~~~~~~~~~~~~
+
+  unsigned int adc_read(unsigned char adc_channel_num)
+{
+  //clear the channel selection bits (MUX 4:0) flip a certain bit
+  //on which register?
+  *my_ADMUX &= 0xE0;
+  // 0000 0000 & 1110 0000 = 0000 0000
+
+  //clear the channel selection bits (MUX 5)
+  //on which register?
+  *my_ADCSRB &= 0xF7;
+  // 0000 0000 & 1111 0111 = 0000 0000
+
+  //set the channel number (which is the input ranging from 0-15)
+  //the last 3 bits in ADMUX determines which pins are going to be used
+  // 000 = channel 0, ..., 111 = channel 7
+  //to change channel, set MUX5 from ADCSRB
+
+  //adc_channel_num = 0;
+
+  if (adc_channel_num > 7)
+  {
+    //set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+
+    //set the MUX bit 5
+    //on which register?
+    //if MUX 5 is set to 0, it accesses A0-A7. If it's set to 1 A8-A15.
+
+    *my_ADCSRB &= 0xF7;
+  // 0000 0000 & 1111 0111 = 0000 0000
+  }
+
+  //set the channel selection bits
+  *my_ADMUX |= adc_channel_num;
+
+  //set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // 0000 0000 | 0100 000 = 0100 0000
+
+  //wait for the conversion to complete
+  while ((*my_ADCSRA & 0x40) != 0);
+
+  //return the result in the ADC data register
+  return *my_ADC_DATA;
+
+  //
+
+
+
+/*
+Ignore these:
+      Serial.println("Increment by 1.");
       //increments the counter by 1
       counter++;
       //1 % 16 = 1, 2 % 16 = 2, ... , 16 % 16 = 0
